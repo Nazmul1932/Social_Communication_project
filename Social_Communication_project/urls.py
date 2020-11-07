ff8d2f1a12cb60a97e39470116b5660be469b18a
Nazmul1932 <46520530+Nazmul1932@users.noreply.github.com>
2020-11-07 00:08:34 -0800

    Initial Commit

--- Social_Communication_project/urls.py
+++ Social_Communication_project/urls.py
@@ -1,14 +1,16 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home_view'),
+    path('profiles/', include('profiles.urls', namespace='profiles')),
+    path('posts/', include('posts.urls', namespace='posts')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
-
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Profile)
+admin.site.register(Relationship)
 
 # Register your models here.
